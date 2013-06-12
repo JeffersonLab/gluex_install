22136d8c9d7c0bddf9ba9892ca968a57c5baa7cf
Mark Ito <marki@jlab.org>
2013-06-12 20:36:25 +0000

    do the source of setup.sh

--- gluex_install.sh
+++ gluex_install.sh
@@ -4,5 +4,6 @@ pushd gluex
 svn checkout https://halldsvn.jlab.org/repos/trunk/scripts/build_scripts
 echo export GLUEX_TOP=`pwd` > setup.sh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.sh >> setup.sh
+source setup.sh
 make -f $BUILD_SCRIPTS/Makefile_all
 popd
