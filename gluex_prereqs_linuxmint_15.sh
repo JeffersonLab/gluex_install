ce238e71c6e20d16702ae5b24aa664ed49943f6e
Mark Ito <marki@jlab.org>
2013-06-18 19:13:41 +0000

    linuxmint 15

---
+++ gluex_prereqs_linuxmint_15.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get -y install subversion g++ gfortran xutils-dev libxt-dev libxft-dev \
+    liblapack-dev libblas-dev libmotif-dev dpkg-dev libxpm-dev libxext-dev \
+    expect libgl1-mesa-dev libmysqlclient-dev tcsh libbz2-dev
+cd /usr/bin; ln -s make gmake
+cd /usr/include; ln -s freetype2/freetype freetype
