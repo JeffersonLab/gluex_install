6d50129a1125ff5ccd0aa2402a2d9e5b28bd30cd
Mark Ito <marki@jlab.org>
2013-06-20 18:26:53 +0000

    rename, add make

--- gluex_prereqs_suse_12.3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-yast -i subversion wget gcc-c++ gcc-fortran imake libXt-devel \
-    openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-connector-c++-devel \
-    libbz2-devel tcsh
-cd /usr/include ; ln -s freetype2/freetype freetype
