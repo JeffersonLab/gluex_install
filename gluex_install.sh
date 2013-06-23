df278ce9f8d5adbdaf54743ae0a63d1200f550af
Mark Ito <marki@jlab.org>
2013-06-23 18:48:01 +0000

    add csh set-up file

--- gluex_install.sh
+++ gluex_install.sh
@@ -2,8 +2,11 @@
 mkdir gluex
 pushd gluex
 svn checkout https://halldsvn.jlab.org/repos/trunk/scripts/build_scripts
-echo export GLUEX_TOP=`pwd` > setup.sh
+pwd_string=`pwd`
+echo export GLUEX_TOP=$pwd_string > setup.sh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.sh >> setup.sh
+echo setenv GLUEX_TOP $pwd_string > setup.csh
+echo source \$GLUEX_TOP/build_scripts/gluex_env.csh >> setup.csh
 source setup.sh
 make -f $BUILD_SCRIPTS/Makefile_all
 popd
