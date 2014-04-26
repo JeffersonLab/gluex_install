8a6aa2cdb1bb44c9d1892aa6eff30e17d9c579ba
Mark Ito <marki@jlab.org>
2014-04-26 12:52:05 +0000

    add freetype links for root

--- gluex_prereqs_ubuntu_14.04_x86_64.sh
+++ gluex_prereqs_ubuntu_14.04_x86_64.sh
@@ -2,10 +2,14 @@
 apt-get -y install subversion g++ gfortran xutils-dev libxt-dev libxft-dev \
     liblapack-dev libblas-dev libmotif-dev dpkg-dev libxpm-dev libxext-dev \
     expect libgl1-mesa-dev libmysqlclient-dev tcsh libbz2-dev
+
 cd /usr/bin; ln -s make gmake
-cd /usr/include
+
+cd /usr/include # for cernlib
 for file in ft2build.h config freetype.h fttypes.h ftsystem.h ftimage.h \
     fterrors.h ftmoderr.h fterrdef.h
 do
     ln -s freetype2/$file .
 done
+
+cd /usr/include/freetype2 ; ln -s ../freetype2 freetype # for ROOT
