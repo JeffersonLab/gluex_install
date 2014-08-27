d6d3973b5c8f2c5383e3ae37663636da63963830
Mark Ito <marki@jlab.org>
2014-08-27 21:23:51 +0000

    add links for libblas3.a and liblapack3.a

--- gluex_prereqs_scientific_linux_6.sh
+++ gluex_prereqs_scientific_linux_6.sh
@@ -3,3 +3,7 @@ yum install -y subversion make wget gcc-c++ gcc-gfortran imake libXt-devel \
     openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-devel \
     bzip2-devel tcsh scons expat-devel
 cd /usr/include ; ln -s freetype2/freetype freetype
+
+cd /usr/lib64
+ln -s -v liblapack.a liblapack3.a
+ln -s -v libblas.a libblas3.a
