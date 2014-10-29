df53dce28bc934355cd4961ff1753c152f4ecf17
Mark Ito <marki@jlab.org>
2014-10-29 22:44:15 +0000

    modernize prerequisites script for scientific Linux 6

--- gluex_prereqs_scientific_linux_6.sh
+++ gluex_prereqs_scientific_linux_6.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-yum install -y subversion make wget gcc-c++ gcc-gfortran imake libXt-devel \
-    openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-devel \
-    bzip2-devel tcsh scons expat-devel
-cd /usr/include ; ln -s freetype2/freetype freetype
+#yum -y install epel-release
+./prereqs_redhat.sh
 
-cd /usr/lib64
-ln -s -v liblapack.a liblapack3.a
-ln -s -v libblas.a libblas3.a
