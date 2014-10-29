4198da28a6d82e898a92d8c255acb4e0bb591f20
Mark Ito <marki@jlab.org>
2014-10-29 21:38:07 +0000

    Define and then use BUILD_SCRIPTS throughout

--- gluex_install.sh
+++ gluex_install.sh
@@ -3,10 +3,17 @@ mkdir -p gluex
 pushd gluex
 svn checkout https://halldsvn.jlab.org/repos/trunk/scripts/build_scripts
 pwd_string=`pwd`
+export GLUEX_TOP=$pwd_string
+export BUILD_SCRIPTS=$GLUEX_TOP/build_scripts
 echo export GLUEX_TOP=$pwd_string > setup.sh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.sh >> setup.sh
 echo setenv GLUEX_TOP $pwd_string > setup.csh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.csh >> setup.csh
-source setup.sh
+rm -fv version.xml
+wget --no-check-certificate https://halldweb1.jlab.org/dist/version.xml
+eval `$BUILD_SCRIPTS/version.pl -sbash version.xml`
+source $BUILD_SCRIPTS/gluex_env.sh
+echo ++++++++++++++++++++++++++++++++++++++++++DEBUG++++++++++++++++++++++++
+printenv
 make -f $BUILD_SCRIPTS/Makefile_all gluex
 popd
