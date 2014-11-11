5321baa3eb59461eccd724a0c6cf7e8b7727afff
Mark Ito <marki@jlab.org>
2014-11-11 23:02:39 +0000

    put common debian packages in one place

--- gluex_prereqs_ubuntu_14.04_x86_64.sh
+++ gluex_prereqs_ubuntu_14.04_x86_64.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-apt-get -y install subversion g++ gfortran xutils-dev libxt-dev libxft-dev \
-    liblapack-dev libblas-dev libmotif-dev dpkg-dev libxpm-dev libxext-dev \
-    expect libgl1-mesa-dev libmysqlclient-dev tcsh libbz2-dev scons
-
-cd /usr/bin; ln -s make gmake
-
-cd /usr/include # for cernlib
-for file in ft2build.h config freetype.h fttypes.h ftsystem.h ftimage.h \
-    fterrors.h ftmoderr.h fterrdef.h
-do
-    ln -s freetype2/$file .
-done
-
-cd /usr/include/freetype2 ; ln -s ../freetype2 freetype # for ROOT
-
-cd /usr/lib
-ln -s liblapack.a liblapack3.a
-ln -s libblas.a libblas3.a
+./prereqs_debian.sh
+apt-get -y install libxml-simple-perl
