a18c56688388d5be1fd5700361a0685df309db20
Mark Ito <marki@jlab.org>
2015-03-11 19:17:10 +0000

    do not overwrite version.xml

--- gluex_install.sh
+++ gluex_install.sh
@@ -5,15 +5,20 @@ svn checkout https://halldsvn.jlab.org/repos/trunk/scripts/build_scripts
 pwd_string=`pwd`
 export GLUEX_TOP=$pwd_string
 export BUILD_SCRIPTS=$GLUEX_TOP/build_scripts
+rm -fv setup.sh
 echo export GLUEX_TOP=$pwd_string > setup.sh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.sh >> setup.sh
+rm -fv setup.csh
 echo setenv GLUEX_TOP $pwd_string > setup.csh
 echo source \$GLUEX_TOP/build_scripts/gluex_env.csh >> setup.csh
-rm -fv version.xml
-wget --no-check-certificate https://halldweb.jlab.org/dist/version.xml
+if [ -f version.xml ]
+    then
+    echo version.xml exists
+else
+    echo getting version.xml from halldweb
+    wget --no-check-certificate https://halldweb.jlab.org/dist/version.xml
+fi
 eval `$BUILD_SCRIPTS/version.pl -sbash version.xml`
 source $BUILD_SCRIPTS/gluex_env.sh
-echo ++++++++++++++++++++++++++++++++++++++++++DEBUG++++++++++++++++++++++++
-printenv
 make -f $BUILD_SCRIPTS/Makefile_all gluex
 popd
