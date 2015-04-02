3617e9a9b6bfd867e4a1e51d95aefa378942ab48
Mark Ito <marki@jlab.org>
2015-04-02 14:52:11 +0000

    remove extraneous rpms

--- gluex_prereqs_opensuse.sh
+++ gluex_prereqs_opensuse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-zypper --non-interactive install make subversion wget gcc-c++ gcc-fortran \
-    imake libXt-devel openmotif-devel lapack-devel blas-devel libXpm-devel \
-    expect mysql-connector-c++-devel libbz2-devel tcsh patch libXp-devel scons \
+zypper --non-interactive install make subversion gcc-c++ gcc-fortran \
+    imake libXt-devel lapack-devel blas-devel libXpm-devel \
+    expect libbz2-devel patch libXp-devel scons \
     libmysqlclient-devel perl-XML-Writer perl-File-Slurp
 cd /usr/include
 ln -s freetype2/freetype freetype
