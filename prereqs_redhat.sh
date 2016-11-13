3c3a9089ca0ddc0dd2539b2b1e4da5f5ebd62c70
Mark Ito <marki@jlab.org>
2016-11-13 17:35:51 -0500

    Remove lapack and blas. Now built from source. Remove expect. Geant4 build no longer depends on it. Add mesa-libGLU-devel. Seems to be needed by Geant4.

--- prereqs_redhat.sh
+++ prereqs_redhat.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
-    openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-devel \
-    bzip2-devel tcsh scons expat-devel lapack-static blas-static \
-    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git
+    openmotif-devel libXpm-devel mysql-devel \
+    bzip2-devel tcsh scons expat-devel \
+    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git mesa-libGLU-devel
 cd /usr/include
 ln -s freetype2/freetype freetype
-cd /usr/lib64
-ln -s -v liblapack.a liblapack3.a
-ln -s -v libblas.a libblas3.a
