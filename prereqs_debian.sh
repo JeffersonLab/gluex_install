1876724ecdb1609c2e9f435c44096b3e7c0a38be
Mark Ito <marki@jlab.org>
2016-12-04 12:19:41 -0500

    Drop installation of lapack and blas packages.

--- prereqs_debian.sh
+++ prereqs_debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -y install subversion gfortran xutils-dev libxt-dev libxft-dev \
-    liblapack-dev libblas-dev libmotif-dev libxpm-dev libxext-dev \
+    libmotif-dev libxpm-dev libxext-dev \
     expect libgl1-mesa-dev libmysqlclient-dev tcsh libbz2-dev scons \
     libxml-simple-perl libxml-writer-perl libfile-slurp-perl git cmake \
     python-dev libglu1-mesa-dev
