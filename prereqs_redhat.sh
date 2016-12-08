46aa29eb5aacdef90ca301e9261b5c1cd23e1a16
Mark Ito <marki@localhost.localdomain>
2016-12-08 10:35:07 -0500

    Add cmake to list of packages.

--- prereqs_redhat.sh
+++ prereqs_redhat.sh
@@ -2,6 +2,7 @@
 yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     openmotif-devel libXpm-devel mysql-devel \
     bzip2-devel tcsh scons expat-devel \
-    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git mesa-libGLU-devel
+    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
+    mesa-libGLU-devel cmake
 cd /usr/include
 ln -s freetype2/freetype freetype
