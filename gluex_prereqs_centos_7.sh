7f20f8aa612fa10c1cddeb9cfb9f0c7088143b4d
markito3 <marki@jlab.org>
2017-04-11 16:00:57 -0400

    Install cmake3 and python-devel.

---
+++ gluex_prereqs_centos_7.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum -y install epel-release
+file=prereqs_redhat.sh
+if [ ! -f $file ]
+    then
+    echo $file not found in current directory
+    exit 1
+fi
+./$file
+yum -y install cmake3
