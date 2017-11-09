747b1dc06aac9f5dac35acc58861ea0a6d216422
markito3 <marki@jlab.org>
2017-11-09 13:30:43 -0500

    add gsl and boost

--- prereqs_fedora.sh
+++ prereqs_fedora.sh
@@ -2,7 +2,7 @@
 dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-devel \
     bzip2-devel tcsh scons expat-devel lapack-static blas-static \
-    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp
+    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp boost-devel gsl-devel
 cd /usr/include
 ln -s freetype2/freetype freetype
 cd /usr/lib64
