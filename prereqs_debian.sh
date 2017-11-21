7021c3e52a6c5372d29769a0d0c6a536625c42e8
Mark Ito <marki@jlab.org>
2017-11-21 18:12:54 -0500

    Add libgsl-dev.

--- prereqs_debian.sh
+++ prereqs_debian.sh
@@ -4,7 +4,7 @@ apt-get -y install subversion gfortran xutils-dev libxt-dev libxft-dev \
     expect libgl1-mesa-dev libmysqlclient-dev tcsh libbz2-dev scons \
     libxml-simple-perl libxml-writer-perl libfile-slurp-perl git cmake \
     python-dev libglu1-mesa-dev libqt4-dev libxmu-dev libboost-dev \
-    libboost-python-dev libxi-dev
+    libboost-python-dev libxi-dev libgsl-dev
 
 cd /usr/bin; ln -s make gmake
 
