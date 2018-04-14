efb8e3a2b4e254aeaa6d2cfa0c806fd0dc23433f
Mark Ito <marki@ltmarki.localdomain>
2018-04-13 21:47:58 -0400

    Add nano to the list of packages.

--- prereqs_redhat.sh
+++ prereqs_redhat.sh
@@ -3,6 +3,6 @@ yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     openmotif-devel libXpm-devel mysql-devel \
     bzip2-devel tcsh scons expat-devel \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
-    mesa-libGLU-devel cmake python-devel qt-devel boost-devel gsl-devel
+    mesa-libGLU-devel cmake python-devel qt-devel boost-devel gsl-devel nano
 cd /usr/include
 ln -s freetype2/freetype freetype
