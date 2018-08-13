885ee53d7cb291a060f17c0a18a7f2607910f439
Sean Dobbs <sdobbs@charm.physics.fsu.edu>
2018-08-13 15:13:22 -0400

    Enable command to work from other directories.

--- gluex_cmds.sh
+++ gluex_cmds.sh
@@ -5,4 +5,4 @@ export GLUEX_INSTALL_HOME=$SCRIPT_DIR
 
 # useful aliases & commands
 #alias gluex_releases='eval "$(python gluex_releases.py $@)"'
-gluex_releases() { if [ "$1" == "config" ]; then eval "$(python gluex_releases.py $1 $2)"; else python gluex_releases.py "$@"; fi }
+gluex_releases() { if [ "$1" == "config" ]; then eval "$(python $GLUEX_INSTALL_HOME/gluex_releases.py $1 $2)"; else current_dir=$PWD;cd $GLUEX_INSTALL_HOME; python gluex_releases.py "$@";cd $current_dir; fi }
