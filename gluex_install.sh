43e49b64894c3b0eb44d1c6fe689e05042f0e819
Mark Ito <marki@jlab.org>
2018-11-25 13:08:00 -0500

    Use version_jlab.xml rather than version.xml. Puts in directory tags so that upgrades will not step on themselves.

--- gluex_install.sh
+++ gluex_install.sh
@@ -17,17 +17,17 @@ export BUILD_SCRIPTS=$GLUEX_TOP/build_scripts
 rm -fv setup.sh
 echo export GLUEX_TOP=$pwd_string > setup.sh
 echo export BUILD_SCRIPTS=\$GLUEX_TOP/build_scripts >> setup.sh
-echo source \$BUILD_SCRIPTS/gluex_env_version.sh $pwd_string/version.xml >> setup.sh
+echo source \$BUILD_SCRIPTS/gluex_env_version.sh $pwd_string/version_jlab.xml >> setup.sh
 rm -fv setup.csh
 echo setenv GLUEX_TOP $pwd_string > setup.csh
 echo setenv BUILD_SCRIPTS \$GLUEX_TOP/build_scripts >> setup.csh
-echo source \$BUILD_SCRIPTS/gluex_env_version.csh $pwd_string/version.xml >> setup.csh
-if [ -f version.xml ]
+echo source \$BUILD_SCRIPTS/gluex_env_version.csh $pwd_string/version_jlab.xml >> setup.csh
+if [ -f version_jlab.xml ]
     then
-    echo version.xml exists, skip download
+    echo version_jlab.xml exists, skip download
 else
-    echo getting version.xml from halldweb.jlab.org
-    wget --no-check-certificate https://halldweb.jlab.org/dist/version.xml
+    echo getting version_jlab.xml from halldweb.jlab.org
+    wget --no-check-certificate https://halldweb.jlab.org/dist/version_jlab.xml
 fi
 source setup.sh
 make -f $BUILD_SCRIPTS/Makefile_all gluex_pass1
