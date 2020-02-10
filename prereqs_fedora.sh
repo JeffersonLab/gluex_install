722f061cb4b07871259dfa600e4dff0f685cee10
markito3 <marki@jlab.org>
2020-02-10 11:20:06 -0500

    Remove blas and lapack from prerequisites for Fedora.

--- prereqs_fedora.sh
+++ prereqs_fedora.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
-    openmotif-devel lapack-devel blas-devel libXpm-devel expect mysql-devel \
+    openmotif-devel libXpm-devel expect mysql-devel \
     bzip2-devel tcsh scons expat-devel lapack-static blas-static \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp boost-devel gsl-devel
 cd /usr/include
 ln -s freetype2/freetype freetype
-cd /usr/lib64
-ln -s -v liblapack.a liblapack3.a
-ln -s -v libblas.a libblas3.a
