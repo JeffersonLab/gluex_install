168c1abc10f9ef7a1b8ed88c60672045ec9443c4
Mark Ito <marki@jlab.org>
2020-05-19 19:37:02 -0400

    do tbb-devel in favor of tbb

--- gluex_prereqs_centos_7.sh
+++ gluex_prereqs_centos_7.sh
@@ -5,6 +5,6 @@ yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     bzip2-devel tcsh scons expat-devel \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
     mesa-libGLU-devel cmake python-devel qt-devel boost-devel gsl-devel \
-    libtool which bc nano cmake3 tbb xrootd-client-libs xrootd-client
+    libtool which bc nano cmake3 tbb-devel xrootd-client-libs xrootd-client
 cd /usr/include
 ln -s freetype2/freetype freetype
