9606c016299dac5537f918e62b1a842839f9f5bf
Mark Ito <marki@jlab.org>
2020-08-12 16:51:41 -0400

    intermediate check-in

--- gluex_prereqs_ubuntu_20.04.sh
+++ gluex_prereqs_ubuntu_20.04.sh
@@ -8,7 +8,7 @@ apt-get -y install gfortran xutils-dev libxt-dev libxft-dev \
     libxml-simple-perl libxml-writer-perl libfile-slurp-perl git cmake \
     python-dev libglu1-mesa-dev libqt5-dev libxmu-dev libboost-dev \
     libboost-python-dev libxi-dev libgsl-dev wget autoconf libtool curl g++ \
-    libtirpc-dev
+    libtbb-dev libtirpc-dev
 
 cd /usr/bin; ln -s make gmake
 
