85f6608e68682b8fba3757211ace982a8267e9bb
Mark Ito <marki@jlab.org>
2020-08-24 12:42:49 -0400

    Add packages python2 and libnsl2.

--- gluex_prereqs_centos_8.sh
+++ gluex_prereqs_centos_8.sh
@@ -7,6 +7,7 @@ dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     bzip2-devel tcsh python3-scons expat-devel \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
     mesa-libGLU-devel cmake python36-devel qt5-devel boost-devel gsl-devel \
-    libtool which bc nano cmake3 tbb-devel xrootd-client-libs xrootd-client
+    libtool which bc nano cmake3 tbb-devel xrootd-client-libs xrootd-client \
+    python2 libnsl2
 cd /usr/include
 ln -s freetype2/freetype freetype
