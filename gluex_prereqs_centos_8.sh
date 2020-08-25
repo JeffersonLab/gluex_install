8dc228ff7fb87c45239298c2b199d8be49028870
Mark Ito <marki@jlab.org>
2020-08-25 10:47:22 -0400

    Add installation of "make".

--- gluex_prereqs_centos_8.sh
+++ gluex_prereqs_centos_8.sh
@@ -8,6 +8,6 @@ dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
     mesa-libGLU-devel cmake python36-devel qt5-devel boost-devel gsl-devel \
     libtool which bc nano cmake3 tbb-devel xrootd-client-libs xrootd-client \
-    python2 libnsl2 make
+    python2 libnsl2-devel make
 cd /usr/include
 ln -s freetype2/freetype freetype
