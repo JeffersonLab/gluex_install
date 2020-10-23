7a595b3798b3ee570a70fbda11701288dfeab5bc
Mark Ito <marki@jlab.org>
2020-10-23 13:49:52 -0400

    Fedora 32 container recipe and GlueX prerequisite install script.

---
+++ gluex_prereqs_fedora_32.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
+    openmotif-devel libXpm-devel mysql-devel \
+    bzip2-devel tcsh scons expat-devel \
+    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
+    mesa-libGLU-devel cmake python-devel qt-devel boost-devel gsl-devel \
+    libtool which bc nano cmake3 libnsl2-devel
+cd /usr/include
+ln -s freetype2/freetype freetype
+dnf install -y findutils
