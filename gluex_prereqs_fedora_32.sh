672c0cdf6032ec31fc1270e98497623e113af685
Mark Ito <marki@jlab.org>
2020-10-23 16:54:31 -0400

    Add python3-future package.

--- gluex_prereqs_fedora_32.sh
+++ gluex_prereqs_fedora_32.sh
@@ -4,7 +4,7 @@ yum install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     bzip2-devel tcsh scons expat-devel \
     perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
     mesa-libGLU-devel cmake python-devel qt-devel boost-devel gsl-devel \
-    libtool which bc nano cmake3 libnsl2-devel python27
+    libtool which bc nano cmake3 libnsl2-devel python27 python3-future
 cd /usr/include
 ln -s freetype2/freetype freetype
 dnf install -y findutils
