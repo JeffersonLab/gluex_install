a5645a66d285ec48101749c689fb12069f5d2f25
Mark Ito <markito3@gmail.com>
2021-08-23 21:23:59 -0400

    Add a usage message and some error checks.

--- build_gluex.sh
+++ build_gluex.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+if [ "$1" == "-h" ]
+then
+    cat <<EOF
+
+usage: build_gluex.sh [-h] [version-set-file]
+
+    -h: print this usage message
+    version-set-file: name of version set file with absolute path or path
+        relative to the current working directory
+
+EOF
+    exit 0
+fi
 version_set=$1
 set --
-source gluex_env_boot.sh
+if ! source gluex_env_boot.sh
+then
+    echo build_gluex.sh error: could not source gluex_env_boot.sh
+    exit 1
+fi
+if [ ! -f $version_set ]
+then
+    echo build_gluex.sh error: could not find version set file $version_set
+    exit 2
+fi
 gxenv $version_set
 make -f $BUILD_SCRIPTS/Makefile_all gluex_pass1
 if [ $? -ne 0 ]
