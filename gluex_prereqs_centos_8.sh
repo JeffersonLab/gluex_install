2a8812997c4cafc17c0e902f3ce757419598df98
Mark Ito <markito3@gmail.com>
2021-12-22 20:16:16 -0500

    Update prereq scripts to include fmt-devel

--- gluex_prereqs_centos_8.sh
+++ gluex_prereqs_centos_8.sh
@@ -11,6 +11,6 @@ dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     mesa-libGLU-devel cmake python36-devel qt5-devel boost-devel gsl-devel \
     libtool which bc nano cmake3 xrootd-client-libs xrootd-client \
     python2 libnsl2-devel make boost-python3-devel libXi-devel python3-future \
-    emacs gdb mariadb xterm python3-devel boost-python3-devel
+    emacs gdb mariadb xterm python3-devel boost-python3-devel fmt-devel
 cd /usr/include
 ln -s freetype2/freetype freetype
