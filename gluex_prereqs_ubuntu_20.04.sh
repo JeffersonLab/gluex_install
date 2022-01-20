53a6715367d11cf046409b77641de5b3b442d5a8
Mark Ito <markito3@gmail.com>
2022-01-20 13:48:33 -0500

    Add xterm and bc. Add steps to allow query of apt database.

--- gluex_prereqs_ubuntu_20.04.sh
+++ gluex_prereqs_ubuntu_20.04.sh
@@ -8,7 +8,9 @@ apt-get -y install gfortran xutils-dev libxt-dev libxft-dev \
     libxml-simple-perl libxml-writer-perl libfile-slurp-perl git cmake \
     python-dev libglu1-mesa-dev qt5-default libxmu-dev libboost-dev \
     libboost-python-dev libxi-dev libgsl-dev wget autoconf libtool curl g++ \
-    libtirpc-dev python3-future libfmt-dev bc
+    libtirpc-dev python3-future libfmt-dev bc xterm emacs
+apt-get install apt-file gdb
+apt-file update
 
 cd /usr/bin; ln -s make gmake
 
