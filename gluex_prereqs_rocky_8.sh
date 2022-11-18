16b3634f5f3a7f2c411614c332935614a0f899cc
Mark Ito <markito3@gmail.com>
2022-11-18 17:46:11 -0500

    add a few packages

---
+++ gluex_prereqs_rocky_8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dnf -y update
+dnf -y install epel-release
+dnf -y install dnf-plugins-core
+dnf config-manager --set-enabled powertools
+dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
+    openmotif-devel libXpm-devel mysql-devel \
+    bzip2-devel tcsh python3-scons expat-devel \
+    perl-XML-Simple perl-XML-Writer patch perl-File-Slurp git \
+    mesa-libGLU-devel cmake python36-devel qt5-devel boost-devel gsl-devel \
+    libtool which bc nano cmake3 xrootd-client-libs xrootd-client \
+    python2 libnsl2-devel make boost-python3-devel libXi-devel python3-future \
+    emacs gdb mariadb xterm python3-devel boost-python3-devel fmt-devel scons \
+    uptime xload xclock xeyes xev gedit
+cd /usr/include
+ln -s freetype2/freetype freetype
