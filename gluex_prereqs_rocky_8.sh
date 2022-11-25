db57339f10a0ae9d3501e1bbd9c9bf963535a6c4
Mark Ito <markito3@gmail.com>
2022-11-24 20:52:37 -0500

    add scons to rocky

--- gluex_prereqs_rocky_8.sh
+++ gluex_prereqs_rocky_8.sh
@@ -10,7 +10,7 @@ dnf install -y subversion wget gcc-c++ gcc-gfortran imake libXt-devel \
     mesa-libGLU-devel cmake python36-devel qt5-devel boost-devel gsl-devel \
     libtool which bc nano cmake3 xrootd-client-libs xrootd-client \
     python2 libnsl2-devel make boost-python3-devel libXi-devel python3-future \
-    emacs gdb mariadb xterm python3-devel boost-python3-devel fmt-devel scons \
-    uptime xload xclock xeyes xev gedit
+    emacs gdb mariadb xterm python3-devel boost-python3-devel fmt-devel \
+    python3-scons procps-ng xload xclock xeyes xev gedit
 cd /usr/include
 ln -s freetype2/freetype freetype
