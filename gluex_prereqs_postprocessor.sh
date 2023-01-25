92141452e43f924e41d93048e22f86d3edf5ed0a
Peter Pauli <Peter.Pauli@glasgow.ac.uk>
2023-01-25 13:04:44 +0000

    Experimental plaster for issue with Docker

--- gluex_prereqs_postprocessor.sh
+++ gluex_prereqs_postprocessor.sh
@@ -2,6 +2,7 @@
 echo 'Set up gxshell...'
 cp /gluex_install/gxshell/gxshell /usr/local/bin/gxshell
 cp /gluex_install/gxshell/gluex-env.sh /etc/gluex-env.sh
+mkdir -p /.singularity.d/env/
 cp /gluex_install/gxshell/10-docker2singularity.sh /.singularity.d/env/10-docker2singularity.sh
 cp /gluex_install/gxshell/99-zz_cntrenv.sh /.singularity.d/env/99-zz_cntrenv.sh
 echo '...done!'
