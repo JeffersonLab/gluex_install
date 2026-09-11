9ed9bf089af01a27c74e73e0651e0176ab4e86dc
Alexander Austregesilo <aaustreg@jlab.org>
2026-09-11 11:58:31 -0400

    easier solution

--- gluex_prereqs_almalinux_9.sh
+++ gluex_prereqs_almalinux_9.sh
@@ -29,6 +29,6 @@ isDockerBuildkit(){
     [[ -f "$cgroup" ]] && grep -q '.*:cpuset:/docker/buildkit/' "$cgroup"
 }
 isKanikoBuild(){
-    grep -q '/kaniko/executor' /proc/1/cmdline
+    [[ -d "/kaniko" ]]
 }
 if isDockerBuildkit || isKanikoBuild; then source /gluex_install/gluex_prereqs_postprocessor.sh ; fi
